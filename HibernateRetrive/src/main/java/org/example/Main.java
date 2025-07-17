b82a8f479b13145f2521bafd8bf7aca237c99c89
Milan Raja <milanraja881@gmail.com>
2025-07-17 16:26:31 +0530

    Hibernate_Retrive

--- HibernateRetrive/src/main/java/org/example/Main.java
+++ HibernateRetrive/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -15,29 +15,21 @@ public class Main {
         s1.setName("Milan Raja");
         s1.setAge(24);
 
-/*
-        Configuration cfg = new Configuration()
-                .configure("hibernate.cfg.xml") // Load hibernate.cfg.xml
-                .addAnnotatedClass(Student.class); // Register entity class
-        //cfg.addAnnotatedClass(Student.class);
-  */
+        Student s2 = null;
+
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(Student.class)
                 .configure()
                 .buildSessionFactory();
 
-
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+        //get => it help us to retrive the data from the database
+        s2 = session.get(Student.class,12);
 
-        //Save is depiricated so we are using persist
-        session.persist(s1);
-
-        transaction.commit();
         session.close();
         sf.close();
+        System.out.println(s2);
 
-        System.out.println(s1);
     }
 }
